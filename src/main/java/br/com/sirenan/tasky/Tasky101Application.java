04680de86d71949e1584f23a4179cd833d262661
silrenan <silrenan@users.noreply.github.com>
2024-09-18 11:24:19 -0300

    commit inicial

---
+++ src/main/java/br/com/sirenan/tasky/Tasky101Application.java
@@ -0,0 +1,13 @@
+package br.com.sirenan.tasky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tasky101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tasky101Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sirenan/tasky/Tasky101ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sirenan.tasky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tasky101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
